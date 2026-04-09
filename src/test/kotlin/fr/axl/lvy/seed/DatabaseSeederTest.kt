8cfb98f7521d707182d84424880532b2c16d0e5b
Axl-Lvy <axl@activeviam.com>
2026-04-09 15:50:23 +0200

    fix seeder test

--- src/test/kotlin/fr/axl/lvy/seed/DatabaseSeederTest.kt
+++ src/test/kotlin/fr/axl/lvy/seed/DatabaseSeederTest.kt
@@ -15,10 +15,12 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @ActiveProfiles("seed")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class DatabaseSeederTest {
 
   @Autowired lateinit var userRepository: UserRepository
